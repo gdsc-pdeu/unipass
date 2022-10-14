115fe2f85c64ef0c56117dda6e37af0f0936085b
rohit.saini1 <rohit.saini@valyu.ai>
2022-10-14 15:14:33 +0530

    add prettier config file

--- utils/createPassword.js
+++ utils/createPassword.js
@@ -1,19 +1,19 @@
-const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'    
-const numbers = '0123456789'
-const symbols = '!@#$%^&*_-+='
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
 
 const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
-    let chars = alpha
-    hasNumbers ? (chars += numbers) : ''
-    hasSymbols ? (chars += symbols) : ''
-    return generatePassword(length, chars)
-}
+  let chars = alpha;
+  hasNumbers ? (chars += numbers) : "";
+  hasSymbols ? (chars += symbols) : "";
+  return generatePassword(length, chars);
+};
 const generatePassword = (length, chars) => {
-    let password = ''
-    for(let i=0; i<length; i++){
-        password += chars.charAt(Math.floor(Math.random() * chars.length))
-    }
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
 
-    return password
-}
-module.exports = createPassword
+  return password;
+};
+module.exports = createPassword;
