67063b6b61ca21578dc1e2b2d1d3fdbf5ef8860e
shahrushin32 <shahrushin32@gmail.com>
2022-10-15 23:51:48 +0530

    added functionality of adding keyword.

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 #!/usr/bin/env node
-const program = require('commander')
-const chalk = require('chalk')
-const clipboardy = require('clipboardy')
-const log = console.log
-const createPassword = require('./utils/createPassword')
-const savePassword = require('./utils/savePassword')
+const program = require("commander");
+const chalk = require("chalk");
+const clipboardy = require("clipboardy");
+const log = console.log;
+const createPassword = require("./utils/createPassword");
+const savePassword = require("./utils/savePassword");
 
-program.version('1.0.0').description('Unique and Simple Password Generator')
+program.version("1.0.0").description("Unique and Simple Password Generator");
 
 program
-.option('-l, --length <number>', 'length of password', '8')
-.option('-s, --save', 'save password to passwords.txt')
-.option('-nn, --no-numbers', 'remove numbers')
-.option('-ns, --no-symbols', 'remove symbols')
-.parse()
+  .option("-l, --length <number>", "length of password", "8")
+  .option("-s, --save", "save password to passwords.txt")
+  .option("-nn, --no-numbers", "remove numbers")
+  .option("-ns, --no-symbols", "remove symbols")
+  .option("-k, --keyword <string>", "add a keyword in your password.", "")
+  .parse();
 
-const {length, save, numbers, symbols} = program.opts()
-
-//console.log(numbers, symbols)
+const { length, save, numbers, symbols, keyword } = program.opts();
+// console.log(length);
+// console.log(numbers, symbols);
+// console.log(keyword);
 
 //get generated password
-const generatedPassword = createPassword(length, numbers, symbols)
+const generatedPassword = createPassword(length, numbers, symbols, keyword);
 
 //Save to file
-if(save) {
-    savePassword(generatedPassword)
+if (save) {
+  savePassword(generatedPassword);
 }
 //copy to clipboard
-clipboardy.writeSync(generatedPassword)
+clipboardy.writeSync(generatedPassword);
 
 //output generated password
-log(chalk.blue('Generated Password: ') + chalk.bold(generatedPassword))
-log(chalk.yellow('Password copied to clipboard'))
+log(chalk.blue("Generated Password: ") + chalk.bold(generatedPassword));
+log(chalk.yellow("Password copied to clipboard"));
--- utils/createPassword.js
+++ utils/createPassword.js
@@ -1,19 +1,30 @@
-const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'    
-const numbers = '0123456789'
-const symbols = '!@#$%^&*_-+='
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
 
-const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
-    let chars = alpha
-    hasNumbers ? (chars += numbers) : ''
-    hasSymbols ? (chars += symbols) : ''
-    return generatePassword(length, chars)
-}
-const generatePassword = (length, chars) => {
-    let password = ''
-    for(let i=0; i<length; i++){
-        password += chars.charAt(Math.floor(Math.random() * chars.length))
-    }
+const createPassword = (
+  length = 8,
+  hasNumbers = true,
+  hasSymbols = true,
+  keyword = ""
+) => {
+  let chars = alpha;
+  hasNumbers ? (chars += numbers) : "";
+  hasSymbols ? (chars += symbols) : "";
+  keyword;
+  return generatePassword(Math.max(length, keyword.length), chars, keyword);
+};
+const generatePassword = (length, chars, keyword) => {
+  let rem_length = length - keyword.length;
+  let password = "";
+  for (let i = 0; i < rem_length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
 
-    return password
-}
-module.exports = createPassword
+  password = password.split("");
+  password.splice(Math.random() * rem_length, 0, keyword);
+  password = password.join("");
+
+  return password;
+};
+module.exports = createPassword;
